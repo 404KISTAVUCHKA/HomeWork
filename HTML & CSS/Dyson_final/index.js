24ad748ec9b99d4fb014f05a875d0acc2e6f1c66
404KISTAVUCHKA <antonivashcenko414@gmail.com>
2026-02-06 22:06:07 +0300

    Add files via upload

---
+++ HTML & CSS/Dyson_final/index.js
@@ -0,0 +1,40 @@
+import HeaderFixed from "./js/header.js";
+import BurgerMenu from "./js/burger.js";
+import Modal from "./js/modal.js";
+
+
+try {
+  const headerFixed = new HeaderFixed({
+    HEADER: "header",
+    HEADER_FIXED: "header--fixed",
+  });
+
+  new BurgerMenu(
+    {
+      BURGER: "burger",
+      BURGER_OPEN: "burger--open",
+      HEADER_MENU: "header__menu",
+      HEADER_MENU_OPEN: "header__menu--open",
+      lABEL: {
+        OPEN: "Открыть меню",
+        CLOSE: "Закрыть меню",
+      },
+      PAGE_BODY: "page__body",
+      PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+      MENU_LINK: "menu__link",
+      BREAKPOINT: 1920,
+      MAIN: "main"
+    },
+    headerFixed
+  );
+
+  new Modal({
+    PAGE_BODY: "page__body",
+    PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+  })
+
+  productSlider();
+  sizes();
+} catch (error) {
+  console.error(error);
+}
