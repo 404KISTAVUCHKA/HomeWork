6285defe56a2c706cca8361229334088f80e13ed
404KISTAVUCHKA <antonivashcenko414@gmail.com>
2026-02-11 13:13:54 +0300

    Add files via upload

---
+++ JS/variables/index.js
@@ -0,0 +1,26 @@
+// Задание 1
+let firstName = "Антон ";
+let lastName = "Иващенко";
+let isStudent = true;
+console.log(firstName + lastName + " " + isStudent)
+
+//Задание 2
+let age = 17;
+let currentYear = 2026;
+let birthYear = currentYear - age;
+console.log(birthYear)
+
+//Задание 3
+console.log(firstName + lastName + ", мне " + age + " лет. Я ученик/ученица курса:" + isStudent)
+
+//Задание 4
+
+//123456falsetrue
+
+let a = "123";
+let b = +"456";
+let c = Number("789");
+let d = Boolean(0);
+let e = Boolean(" ");
+let result = a + b + c + d + e;
+console.log(result);
